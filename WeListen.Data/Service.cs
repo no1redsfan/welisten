82a8ad08fb26107dd18ed4995da37034d0017c17
mcdonoje <mcdonoje@ucmail.uc.edu>
2013-11-22 17:01:37 -0500

    initial import

---
+++ WeListen.Data/Service.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace WeListen.Data
+{
+    /// <summary>
+    ///     A utility class for data access.
+    /// </summary>
+    public class Service : IDisposable
+    {
+        private readonly WeListenEntityModel _context;
+        private bool _isDisposed;
+
+        /// <summary>
+        ///     Creates a new instance of the <see cref="Service" /> class.
+        /// </summary>
+        public Service()
+        {
+            _context = new WeListenEntityModel();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Gets the favorite songs for a user.
+        /// </summary>
+        /// <param name="userName">The user name of the user.</param>
+        /// <returns>All the favorite songs for a user, or <c>null</c> if none are found.</returns>
+        public ICollection<FavoriteSong> GetFavoriteSongsForUser(string userName)
+        {
+            return (from u in _context.Users where u.UserName == userName select u).Single().FavoriteSongs;
+        }
+
+        /// <summary>
+        /// Saves the user.
+        /// </summary>
+        /// <param name="user">The user to save.</param>
+        /// <returns><c>true</c> if the save was successful; otherwise, <c>false</c>.</returns>
+        public bool SaveUser(User user)
+        {
+            this._context.Entry(user).State = user.UserId == 0 ? EntityState.Added : EntityState.Modified;
+            try
+            {
+                this._context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="isDisposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (isDisposing)
+            {
+                // release managed resources
+                if (_context != null)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            _isDisposed = true;
+        }
+    }
+}
---
+++ WeListen.Trial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeListen.Data;
+
+namespace WeListen.Trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*using (var dataService = new Service())
+            {
+                var favoriteSongs = dataService.GetFavoriteSongsForUser("Jimmy");
+                foreach (var favoriteSong in favoriteSongs)
+                {
+                    Console.WriteLine("{0} by {1}", favoriteSong.Song.Title, favoriteSong.Song.Artist.Name);
+                }
+
+
+                dataService.SaveUser(new User
+                {
+                    Email = "bjorg.prodan@uc.edu",
+                    FirstName = "Bjorg",
+                    LastName = "Prodan",
+                    Password = "password",
+                    UserName = "bjorg"
+                });
+            }*/
+
+            Console.ReadLine();
+
+        }
+    }
+}
