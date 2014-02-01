f10117de0edc6a0c79757e98e9a031006441181b
mcdonoje <mcdonoje@ucmail.uc.edu>
2014-02-01 11:31:03 -0500

    Authenticate api method

--- WeListen.Web/API Controllers/UsersController.cs
+++ WeListen.Web/API Controllers/UsersController.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Routing;
+using WeListen.Data;
 
 namespace WeListen.Web.API_Controllers
 {
     public class UsersController : ApiController
     {
+
+        private readonly Service _dataService = new Service();
+
         // GET api/users
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
+
+        [Route("{username}")]
+        public bool GetAuthenticatedUser(string username, string password)
+        {
+            return (_dataService.AuthenticateUser(username, password));
+        }
+
+
 
         // GET api/users/5
         public string Get(int id)
